cd8da45af77e7fdc4f5267f224fa27860bafc265
VijayaKumarGavara <ggana6947@gmail.com>
2026-02-28 16:42:36 +0530

    feature/ updated the database from local to mongo db atlas to cluster0 and upload the data as well & make things work accordingly

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
+
+const db_url=process.env.MONGO_DB_URL;
+const local_db_url=process.env.LOCAL_MONGO_DB_URL;
 
 async function ConnectDB() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/Dhanya-Sethu");
+    await mongoose.connect(db_url);
   } catch (error) {
     console.log("Error while connecting Database,", error);
   }
